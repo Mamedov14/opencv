0e7c27ccf44cca5b711edf04b03dcf00bf523b6a
Vagif <vagif_m@bk.ru>
2022-01-05 15:17:16 +0300

    JavaCV
    Случайно наткнулся на какую-то новую библиотеку, она очень сильно похожа на opencv и наверное это её какой-то собрат))
    с opencv работает отлично, но так же имеет свои мощные методы
    update:
    реализован захват камеры и распознавание лица

---
+++ JavaCV/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.FrameRecorder;
+
+public class Main {
+    public static void main(String[] args) throws FrameGrabber.Exception, FrameRecorder.Exception {
+        new Vision();
+    }
+}
---
+++ JavaCV/src/main/java/Vision.java
@@ -0,0 +1,110 @@
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacv.*;
+
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_objdetect.*;
+
+
+public class Vision {
+
+    public static final String XML_FILE = "H:\\HDD\\Java Book\\opencv_java\\haarcascade_frontalface_default.xml";
+
+    CvHaarClassifierCascade classifierFace = null;
+    int count = 0;
+
+    public Vision() throws FrameGrabber.Exception, FrameRecorder.Exception {
+
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+        IplImage img = null;
+        classifierFace = new CvHaarClassifierCascade(cvLoad(XML_FILE));
+
+        // захват камеры
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(1);
+
+        // подключение к камере через rtsp
+        // FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("rtsp://");
+
+        // подключить видосик
+        //FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("H:\\HDD\\Java Book\\opencv_java\\opencv.avi");
+
+        grabber.start();
+
+        Frame frame = grabber.grab();
+        CanvasFrame canvasFrame = new CanvasFrame("Title");
+        canvasFrame.setCanvasSize(frame.imageWidth, frame.imageHeight);
+
+        // сохранять видосы
+//        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder("H:\\HDD\\Java Book\\opencv_java\\opencv.avi",
+//                frame.imageWidth, frame.imageHeight);
+//        recorder.setFrameRate(25);
+//        recorder.setVideoCodec(13);
+//        recorder.setFormat("avi");
+//        double quality = 1;
+//        recorder.setVideoBitrate((int) (quality * 1024 * 1024));
+//        recorder.start();
+
+        while (canvasFrame.isVisible() && (frame = grabber.grab()) != null) {
+//            img = converter.convert(frame);
+            img = toGray(converter.convert(frame));
+            // IplImage resizeImage = getSubImageFromIpl(img, 100, 100, 200, 200);
+
+            // поиск лица
+            findObject(img);
+            canvasFrame.showImage(converter.convert(img));
+//          recorder.record(converter.convert(img));
+        }
+
+//        recorder.stop();
+//        recorder.close();
+        canvasFrame.dispose();
+    }
+
+    // поиск всех лиц
+    public IplImage getSubImageFromIpl(IplImage img, int x, int y, int w, int h) {
+        IplImage resizeImage = IplImage.create(w, h, img.depth(), img.nChannels());
+        cvSetImageROI(img, cvRect(x, y, w, h));
+        cvCopy(img, resizeImage);
+        cvResetImageROI(img);
+        return resizeImage;
+    }
+
+    // сохранение вырезаного изображения в определённом размере
+    public IplImage resizeIplImage(IplImage img, int w, int h) {
+        IplImage resizeImage = IplImage.create(w, h, img.depth(), img.nChannels());
+        cvResize(img, resizeImage);
+        return resizeImage;
+    }
+
+
+    // поиск границы лица
+    public void findObject(opencv_core.IplImage currentFrame) {
+        CvMemStorage storage = CvMemStorage.create();
+        opencv_core.CvSeq faces = cvHaarDetectObjects(currentFrame, classifierFace, storage, 1.5, 3, CV_HAAR_MAGIC_VAL);
+        int total = faces.total();
+        int face_w = 70;
+        int face_h = 80;
+        if (total > 0) {
+            System.out.println(total + " faces");
+            for (int i = 0; i < total; i++) {
+                opencv_core.CvRect rect = new opencv_core.CvRect(cvGetSeqElem(faces, 1));
+                int x = rect.x();
+                int y = rect.y();
+                int w = rect.width();
+                int h = rect.height();
+//                IplImage face = getSubImageFromIpl(currentFrame, x, y, w, h);
+//                face = resizeIplImage(face, face_w, face_h);
+//                cvSaveImage("C:\\Users\\vagif\\IdeaProjects\\untitled\\src\\main\\resources\\face" + count + "-new.jpg", face);
+//                count++;
+                rectangle(cvarrToMat(currentFrame), new Rect(x, y, w, h), new Scalar(0, 255, 0, 0), 2, 0, 0);
+            }
+        }
+    }
+
+    public IplImage toGray(IplImage img) {
+        IplImage currentFrame = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 1);
+        cvCvtColor(img, currentFrame, CV_RGBA2GRAY);
+        return currentFrame;
+    }
+
+}
